8fec5ca0144c6ee83cdddaf65e779eba31eea601
Kenny Yang <kennyyang0211@gmail.com>
2026-09-24 06:33:48 +0900

    Load versioned application and worker assets after updates

--- assets/pathfinder/layout-client.js
+++ assets/pathfinder/layout-client.js
@@ -1,6 +1,7 @@
 (function(root){'use strict';
+  const assetVersion=new URL(document.currentScript.src).searchParams.get('v');
   class LayoutClient{
-    constructor(url){this.url=url;this.version=0;this.worker=null;this.reject=null;}
+    constructor(url){this.url=new URL(url,location.href);if(assetVersion)this.url.searchParams.set('v',assetVersion);this.version=0;this.worker=null;this.reject=null;}
     cancel(){this.version++;if(this.worker){this.worker.terminate();this.worker=null;}if(this.reject){this.reject(new DOMException('새 입력으로 계산 취소','AbortError'));this.reject=null;}}
     calculate(input,onProgress){this.cancel();const version=this.version;return new Promise((resolve,reject)=>{
       this.reject=reject;const worker=this.worker=new Worker(this.url);
--- assets/pathfinder/layout-worker.js
+++ assets/pathfinder/layout-worker.js
@@ -1,2 +1,2 @@
-importScripts('turf-7.1.0.min.js','layout-core.js');
+importScripts('turf-7.1.0.min.js','layout-core.js'+self.location.search);
 self.onmessage=function(event){const input=event.data;const result=PFLayout.calculate(input,turf,progress=>self.postMessage({type:'progress',inputVersion:input.inputVersion,progress}));self.postMessage({type:'result',inputVersion:input.inputVersion,result});};
